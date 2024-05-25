955f908bc134441a940a8fe5315bf10d8db3646b
harsha9724 <harshahr9724@gmail.com>
2024-05-25 17:46:52 +0530

    dockerized the application

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { body, validationResult } = require("express-validator");
 const User = require("../models/user");
 var jwt = require('jsonwebtoken');
